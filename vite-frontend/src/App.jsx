a17394717d5b79dac40445ea643bf5cbd35036e5
keinberger <philipp.keinberger@gmail.com>
2023-09-23 16:53:56 -0400

    feat: add vite frontend

---
+++ vite-frontend/src/App.jsx
@@ -0,0 +1,33 @@
+import { createWeb3Modal, defaultWagmiConfig } from "@web3modal/wagmi/react"
+import { WagmiConfig } from "wagmi"
+import { arbitrum, mainnet } from "wagmi/chains"
+import { Routes, Route, BrowserRouter } from "react-router-dom"
+
+import Home from "./pages/Home"
+import TeamOverview from "./pages/team/"
+import SetUp from "./pages/set-up"
+
+// 1. Get projectId
+const projectId = "dfe74e8562f98b471672c642ff618ef4"
+
+// 2. Create wagmiConfig
+const chains = [mainnet, arbitrum]
+const wagmiConfig = defaultWagmiConfig({ chains, projectId, appName: "Web3Modal" })
+
+// 3. Create modal
+createWeb3Modal({ wagmiConfig, projectId, chains })
+
+export default function App() {
+	return (
+		<WagmiConfig config={wagmiConfig}>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/set-up" element={<SetUp />} />
+					<Route path="/team/:address" element={<TeamOverview />} />
+				</Routes>
+			</BrowserRouter>
+			{/* <Home /> */}
+		</WagmiConfig>
+	)
+}
---
+++ vite-frontend/src/components/ConnectButton.jsx
@@ -0,0 +1,7 @@
+export default function ConnectButton() {
+	return (
+		<>
+			<w3m-button />
+		</>
+	)
+}

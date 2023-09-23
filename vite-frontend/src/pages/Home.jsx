6a7b50e6ccacd6e621dc5660ac705a12f46f4369
Aaron Kigler <aaronkigler@gmail.com>
2023-09-23 18:10:03 -0400

    ak2

--- vite-frontend/src/pages/Home.jsx
+++ vite-frontend/src/pages/Home.jsx
@@ -1,3 +1,24 @@
+import Header from "./../components/Header";
+
 export default function Home() {
-	return <div className="text-4xl font-extrabold text-black">Home</div>
+  return (
+    <main className="font-inter flex min-h-screen flex-col items-center p-24 justify-start">
+      {/* <div className="flex justify-between w-full mb-6"> */}
+      {/* <div className="text-4xl font-extrabold">Team Name</div> */}
+      <Header />
+      {/* </div> */}
+      <div className="z-10 w-full text-sm flex flex-col items-center justify-center flex-grow">
+        <div className="flex flex-col items-start">
+          <div className="text-4xl font-extrabold">
+            Automatically deploy your Smart Contracts.
+            <br />
+            Gasless. Safe.
+          </div>
+          <div className="text-2xl">
+            And you don’t have to worry about private keys
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 }

ac115cd1dc7714d356c05259ffc430060873e55a
Aaron Kigler <aaronkigler@gmail.com>
2023-09-23 20:18:51 -0400

    homepage

--- vite-frontend/src/components/Header.jsx
+++ vite-frontend/src/components/Header.jsx
@@ -1,18 +1,12 @@
-import ConnectButton from "./ConnectButton"
+import ConnectButton from "./ConnectButton";
 
 export default function Header() {
-	return (
-		<header>
-			<div className="flex mx-[20%] py-[4rem] ">
-				<div className="justify-between flex w-full">
-					<div>
-						<h1 className="text-[36px] font-bold text-black">TeamName</h1>
-					</div>
-					<div>
-						<ConnectButton />
-					</div>
-				</div>
-			</div>
-		</header>
-	)
+  return (
+    <header className="">
+      <div className="flex justify-between">
+        <h1 className="text-[36px] font-bold text-black">TeamName</h1>
+        <ConnectButton />
+      </div>
+    </header>
+  );
 }

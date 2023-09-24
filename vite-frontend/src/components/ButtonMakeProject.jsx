d2f4fa57f3cfa2e83da96ec813bb026270ba2fa0
Aaron Kigler <aaronkigler@gmail.com>
2023-09-23 22:59:03 -0400

    landing page

--- vite-frontend/src/components/ButtonMakeProject.jsx
+++ vite-frontend/src/components/ButtonMakeProject.jsx
@@ -1,5 +1,29 @@
-const Button = () => {
-  return <button></button>;
+import React from "react";
+
+// Styles for the button
+const buttonStyles = {
+  padding: "10px 20px",
+  border: "none",
+  borderRadius: "5px",
+  fontSize: "16px",
+  cursor: "pointer",
+  transition: "background-color 0.3s",
+};
+
+const ButtonMakeProject = ({
+  label,
+  onClick,
+  color = "blue",
+  textColor = "white",
+}) => {
+  return (
+    <button
+      style={{ ...buttonStyles, backgroundColor: color, color: textColor }}
+      onClick={onClick}
+    >
+      {label}
+    </button>
+  );
 };
 
 export default Button;
--- vite-frontend/src/pages/Home.jsx
+++ vite-frontend/src/pages/Home.jsx
@@ -1,29 +1,38 @@
 import Header from "../components/Header";
-import ButtonMakeProject from "../components/ButtonMakeProject";
 import ConnectButton from "../components/ConnectButton";
+import { useAccount } from "wagmi";
+import { Link, useLocation } from "react-router-dom";
 
 export default function Home() {
+  const { address, isConnected } = useAccount();
   return (
     <main className="flex flex-col justify-center h-screen">
-      {/* <div className="flex justify-between w-full mb-6"> */}
-      {/* <div className="text-4xl font-extrabold">Team Name</div> */}
       <div></div>
       <Header />
       {/* </div> */}
-      <div className="z-10 w-full text-sm flex flex-col items-center justify-center flex-grow">
-        <div className="flex flex-col items-start">
-          <div className="text-4xl font-extrabold">
+      <div className="z-10 w-full text-sm flex flex-col items-center  flex-grow">
+        <div className="flex flex-col items-start mt-[150px]">
+          <h1 className="font-bold text-[40px] leading-relaxed">
             Automatically deploy your Smart Contracts.
             <br />
             Gasless. Safe.
-          </div>
-          <div className="text-2xl">
+          </h1>
+          <div className="text-[#777777] text-[20px]">
             And you don’t have to worry about private keys
           </div>
         </div>
         <div className="flex flex-col items-center justify-center mt-3">
-          <ConnectButton />
-          <div> </div>
+          <div className="mt-[70px]">
+            <ConnectButton />
+          </div>
+          <Link className="mt-[15px]" to={"/set-up"}>
+            <button className="mt-3 flex flex-row items-center rounded-xl font-semibold bg-[#000000] text-[10px] text-white drop-shadow-lg px-[23px] py-[8px]">
+              Create Project
+            </button>
+          </Link>
+          <div>
+            <button className="bg-[#3574F4] rounded-lg"></button>
+          </div>
         </div>
       </div>
     </main>
